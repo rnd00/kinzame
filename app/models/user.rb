2e6f89476fe04571563d9fa741fdcfa5f482e099
Adil Omary <adilomary@hotmail.fr>
2020-02-24 18:46:08 +0900

    adding validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :lender, presence: true
+  validates :wallet, numericality: { only_integer: true }
 end
