8479e4a6283a7beba6f8440f500e26c0b520ea3b
Adil Omary <adilomary@hotmail.fr>
2020-02-25 16:04:32 +0900

    finish added methods and authorizations to contract model and controller

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,6 +1,7 @@
 class ContractsController < ApplicationController
-  before_action :set_contract, only: [:show, :edit, :update, :repaid_loan!, :approve!, :repaid_lender!]
+  before_action :set_contract, only: [:show, :edit, :update, :repaid_loan, :approve, :repaid_lender]
   before_action :set_loan, only: [:new, :create, :edit, :update]
+  skip_before_action :authenticate_user!, only: %i(index)
   #to do: add actions for sorting
 
   def index
@@ -8,14 +9,17 @@ class ContractsController < ApplicationController
   end
 
   def show
+    authorize @contract
   end
 
   def new
     @contract = Contract.new
+    authorize @contract
   end
 
   def create
     @contract = Contract.new(contract_params)
+    authorize @contract
     @contract.loan = @loan
     @contract.user = current_user
     if @contract.save
@@ -26,9 +30,11 @@ class ContractsController < ApplicationController
   end
 
   def edit
+    authorize @contract
   end
 
   def update
+    authorize @contract
     if @contract.update(contract_params)
       redirect_to loan_contract_path(id: @contract.id)
     else
@@ -36,17 +42,20 @@ class ContractsController < ApplicationController
     end
   end
 
-  def repaid_loan!
+  def repaid_loan
+    authorize @contract
     @contract.repaid_loan!
     redirect_to loan_contracts_path
   end
 
-  def approve!
+  def approve
+    authorize @contract
     @contract.approve!
     redirect_to loan_contracts_path
   end
 
   def repaid_lender
+    authorize @contract
     @contract.repaid_lender!
     redirect_to loan_contracts_path
   end
--- app/policies/contract_policy.rb
+++ app/policies/contract_policy.rb
@@ -9,7 +9,29 @@ class ContractPolicy < ApplicationPolicy
   # Show - A user should be able to see ONE of his contracts.
   # New - You must be a lender to create a contract AND you must dont have active contracts.
   # Create - ( && user.no_loan?)
-  # Edit -
+  # approve! - Only a lender can approve his own contract.
+  # repaid_lender - Only a borrower can approve his own contract(attached to him)
+  # repaid_loan - Only a lender can approve his own contract.
+
+  def show?
+    record.user == user
+  end
+
+  def create?
+    user.lender && user.no_loan?
+  end
+
+  def approve?
+    user.lender && record.loan.user == user
+  end
+
+  def repaid_lender?
+    record.user == user
+  end
+
+  def repaid_loan?
+    user.lender && record.loan.user == user
+  end
 
 end
 
