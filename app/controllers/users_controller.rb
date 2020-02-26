eb04e05040e8eb7eb5defa444c32bc75127432aa
Brian Heise <bnheise@gmail.com>
2020-02-26 12:52:58 +0900

    default order of contracts is by due_date)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def contracts_index
-    @contracts = current_user.lender ? current_user.loan_contracts : current_user.contracts
+    @contracts = (current_user.lender ? current_user.loan_contracts : current_user.contracts).order(due_date: :desc)
     authorize @contracts
   end
 end
