a7de9b83e03c5f9c5b93484545904cae23d2ccce
Brian Heise <bnheise@gmail.com>
2020-02-26 13:04:22 +0900

    contracts index displays pending loans at the top in separate group

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def contracts_index
     @contracts = (current_user.lender ? current_user.loan_contracts : current_user.contracts).order(:due_date)
     @loans = current_user.loans
+    @pending = @contracts.where(approved: false)
     authorize @contracts
   end
 end
