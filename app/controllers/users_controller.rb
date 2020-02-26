3ed0a227793aa80d9431afd1cdb8feec79725328
Brian Heise <bnheise@gmail.com>
2020-02-26 20:21:59 +0900

    changed user controller so that pending contracts are determined by accepted and rejected status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def dashboard
     @contracts = (current_user.lender ? current_user.loan_contracts : current_user.contracts).order(:due_date)
     @loans = current_user.loans
-    @pending = @contracts.where(approved: false)
+    @pending = @contracts.where(approved: false, rejected: false)
     @completed = @contracts.where(repaid_loan: true)
     @paid = @contracts.where(repaid_lender: true)
     authorize @contracts
