2b4b74ef7f982f9cdd5fab46f83fded970f2a03f
Ryanda Diedan <rndmx00@gmail.com>
2020-02-26 13:14:14 +0900

    hotfix typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @loans = current_user.loans
     @pending = @contracts.where(approved: false)
     @completed = @contracts.where(repaid_loan: true)
-    @paid = @contract.where(repaid_lender: true)
+    @paid = @contracts.where(repaid_lender: true)
     authorize @contracts
   end
 end
