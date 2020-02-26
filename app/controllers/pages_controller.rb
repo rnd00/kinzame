970e040c7290fae4eaee50c7f70472e8dcda5534
Ryanda Diedan <rndmx00@gmail.com>
2020-02-26 16:59:47 +0900

    fastfix banner on home / root

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @tagline = ["Don't get bitten... Borrow locally!",
+                "Snap-up a cheap loan!",
+                "Take a bite out of debt!",
+                "Afraid not-you’re a-loan in this endeavor!",
+                "Get your head above water!"]
   end
 end
