9663ecb344784ce43572bb90c27dc83dc2eabed6
Adil Omary <adilomary@hotmail.fr>
2020-02-26 19:29:48 +0900

    Added Admin Value to False for Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_071059) do
+ActiveRecord::Schema.define(version: 2020_02_26_102813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_071059) do
     t.string "last_name"
     t.integer "wallet", default: 0
     t.boolean "lender", default: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
