5f5e4eb40fe4ddb1f036fcc03db01928e37ee12d
Adil Omary <adilomary@hotmail.fr>
2020-02-27 10:27:50 +0900

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,16 @@ class User < ApplicationRecord
   def no_loan?
     self.contracts.where(repaid_loan: true).empty?
   end
+
+  def money_available
+    # Select the user's wallet
+    # Setup a money_lend variable with inside => iteration of addition of every loan.amount
+    # select actual wallet's value and substract money_lend
+    money_lend = 0
+
+    current_user.loans do |loan|
+      money_lend += loan.amount.where(repaid_lender = false)
+    end
+    current_user.wallet - money_lend
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_104104) do
     t.string "last_name"
     t.integer "wallet", default: 0
     t.boolean "lender", default: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
